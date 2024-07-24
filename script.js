b73556d04b5f345086a06ab5f17040adec7ad3dd
BrunoMaiaPinto <brunommpinto@sapo.pt>
2024-07-24 16:24:31 +0100

    Initial commit

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
 const year = new Date().getFullYear();
 
-document.getElementById(
-  "footer"
-).innerHTML = `Copyright &copy ${year} by Bruno Pinto. All rights reserved.`;
+document.getElementById("year").innerHTML = `${year}`;
